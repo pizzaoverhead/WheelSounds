028ea6f5c34f35bb43830ddde38efb54088ece73
Pizzaoverhead <pizzaoverhead@gmail.com>
2014-01-11 16:47:53 +0000

    0.4 release. Added a sound on opening/closing the landing gear.

--- WheelSounds/WheelSounds.cs
+++ WheelSounds/WheelSounds.cs
@@ -124,3 +124,113 @@ public class WheelSounds : PartModule
             Debug.LogError("WheelSounds on update: Component was null");
     }
 }
+
+
+public class GearSounds : PartModule
+{
+    [KSPField]
+    public float gearSoundVolume = 1f;
+    [KSPField]
+    public string gearSoundFile = "WheelSounds/Sounds/nnus-gear";
+    public FXGroup GearSound = null;
+
+    private ModuleLandingGear.GearStates previousState;
+    private ModuleLandingGear _landingGearModule = null;
+    private ModuleLandingGear landingGearModule
+    {
+        get
+        {
+            if (this._landingGearModule == null)
+                this._landingGearModule = (ModuleLandingGear)this.part.Modules["ModuleLandingGear"];
+            return this._landingGearModule;
+        }
+    }
+    public override void OnStart(StartState state)
+    {
+        if (state == StartState.Editor || state == StartState.None) return;
+
+        if (!GameDatabase.Instance.ExistsAudioClip(gearSoundFile))
+        {
+            Debug.LogError("GearSounds: Audio file not found: " + gearSoundFile);
+            return;
+        }
+
+        if (GearSound == null)
+        {
+            Debug.LogError("GearSounds: Component was null");
+            return;
+        }
+        else
+        {
+            GearSound.audio = gameObject.AddComponent<AudioSource>();
+            GearSound.audio.clip = GameDatabase.Instance.GetAudioClip(gearSoundFile);
+            GearSound.audio.dopplerLevel = 0f;
+            GearSound.audio.Stop();
+            GearSound.audio.loop = false;
+            GearSound.audio.volume = gearSoundVolume;
+            // For some reason these clips aren't starting at the beginning for parts other than the first one.
+            GearSound.audio.time = 0;
+
+            // Seek to a random position in the sound file so we don't have harmonic effects with other wheels.
+            GearSound.audio.time = UnityEngine.Random.Range(0, GearSound.audio.clip.length);
+        }
+
+        GameEvents.onGamePause.Add(new EventVoid.OnEvent(this.OnPause));
+        GameEvents.onGameUnpause.Add(new EventVoid.OnEvent(this.OnUnPause));
+
+        previousState = landingGearModule.gearState;
+    }
+
+    void OnPause()
+    {
+        GearSound.audio.Stop();
+    }
+
+    void OnUnPause()
+    {
+        GearSound.audio.volume = gearSoundVolume;
+    }
+
+    void OnDestroy()
+    {
+        GameEvents.onGamePause.Remove(new EventVoid.OnEvent(OnPause));
+        GameEvents.onGameUnpause.Remove(new EventVoid.OnEvent(OnUnPause));
+    }
+
+    public override void OnUpdate()
+    {
+        if (GearSound != null)
+        {
+            try
+            {
+                if (!GearSound.audio.isPlaying)
+                {
+                    if (previousState == ModuleLandingGear.GearStates.DEPLOYED)
+                    {
+                        if (landingGearModule.gearState == ModuleLandingGear.GearStates.RETRACTING ||
+                            landingGearModule.gearState == ModuleLandingGear.GearStates.RETRACTED)
+                        {
+                            GearSound.audio.Play();
+                        }
+                    }
+                    else if (previousState == ModuleLandingGear.GearStates.RETRACTED)
+                    {
+                        if (landingGearModule.gearState == ModuleLandingGear.GearStates.DEPLOYING ||
+                        landingGearModule.gearState == ModuleLandingGear.GearStates.DEPLOYED)
+                        {
+                            GearSound.audio.time = 0;
+                            GearSound.audio.Play();
+                        }
+                    }
+                }
+                previousState = landingGearModule.gearState;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("WheelSounds: " + ex.Message);
+            }
+        }
+        else
+            Debug.LogError("WheelSounds on update: Component was null");
+    }
+}
